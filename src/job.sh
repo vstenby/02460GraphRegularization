2d04f8d0e3fd6fa26520a75285414ec5325194af
Rasmus Aagaard <raagaard97@gmail.com>
2022-03-27 13:45:14 +0200

    updated job script

--- src/job.sh
+++ src/job.sh
@@ -3,13 +3,13 @@
 ### –- specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J testjob
+#BSUB -J 02460P-reg
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 0:05
+#BSUB -W 24:00
 # request 5GB of system-memory
 #BSUB -R "rusage[mem=5GB]"
 ### -- set the email address --
@@ -33,4 +33,6 @@ module load cuda/10.2
 /appl/cuda/10.2/samples/NVIDIA_CUDA-10.2_Samples/bin/x86_64/linux/release/deviceQuery
 
 #Activate the virtual conda environment and run the script.
-
+source /zhome/55/6/117776/.bashrc
+conda activate preggpu
+wandb agent wakewordteam/02460AdvancedML/yjhb5onh
