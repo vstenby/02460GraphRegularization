f7ff4b4984fc17ec794866fc54d702420519017a
vstenby <s174483@student.dtu.dk>
2022-04-28 14:00:26 +0200

    Adding updated GCN :-)

--- src/GCN.py
+++ src/GCN.py
@@ -6,10 +6,10 @@ class GCN(torch.nn.Module):
     '''
     GCN 
     '''
-    def __init__(self, num_node_features, num_classes):
+    def __init__(self, num_node_features, num_classes, num_hidden_features=16):
         super().__init__()
-        self.conv1 = GCNConv(num_node_features, 16)
-        self.conv2 = GCNConv(16, num_classes)
+        self.conv1 = GCNConv(num_node_features, num_hidden_features)
+        self.conv2 = GCNConv(num_hidden_features, num_classes)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
